2f6ad27058b45e17cb1ab8cb68392634e2d8da5d
Çağrı Uysal <112582764+Tuireol@users.noreply.github.com>
2023-11-06 20:17:19 +0300

    Add tie condition

--- script.js
+++ script.js
@@ -8,34 +8,38 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   let feedback;
-  if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      feedback = "you lost";
+  if (playerSelection === computerSelection) {
+    feedback = "tie";
+  } else {
+    if (playerSelection === "rock") {
+      if (computerSelection === "paper") {
+        feedback = "you lost";
+      }
+      if (computerSelection === "scissors") {
+        feedback = "you won";
+      }
     }
-    if (computerSelection === "scissors") {
-      feedback = "you won";
+    if (playerSelection === "paper") {
+      if (computerSelection === "scissors") {
+        feedback = "you lost";
+      }
+      if (computerSelection === "rock") {
+        feedback = "you won";
+      }
     }
-  }
-  if (playerSelection === "paper") {
-    if (computerSelection === "scissors") {
-      feedback = "you lost";
-    }
-    if (computerSelection === "rock") {
-      feedback = "you won";
-    }
-  }
-  if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      feedback = "you lost";
-    }
-    if (computerSelection === "paper") {
-      feedback = "you won";
+    if (playerSelection === "scissors") {
+      if (computerSelection === "rock") {
+        feedback = "you lost";
+      }
+      if (computerSelection === "paper") {
+        feedback = "you won";
+      }
     }
   }
   return feedback;
 }
 
-const playerSelection = "rock";
-console.log("player selection is rock");
+const playerSelection = "scissors";
+console.log("player selection is scissors");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
